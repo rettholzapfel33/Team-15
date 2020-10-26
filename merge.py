d32b9784584a24ce2a42f95230b6d57116d79606
Rett Holzapfel <rettholzapfel@gmail.com>
2020-10-26 18:37:01 -0500

    Adding program to merge data sets

---
+++ merge.py
@@ -0,0 +1,16 @@
+# This program will merge the data from the Questionnaire data set to the Physiological data set
+import pandas as pd 
+import numpy as np
+import os
+
+# Features you want to merge with Physiological Data
+# Keep Participant ID to merge successfully
+desired_features = ['Participant_ID', 'Age', 'Gender']
+
+df1 = pd.read_csv('Physiological Data.csv')
+df2 = pd.read_csv('Questionnaire Data.csv', usecols=desired_features)
+
+# Merge Gender and Age to DF1
+df3 = pd.merge(df1, df2)
+
+df3.to_csv('Merged.csv')
