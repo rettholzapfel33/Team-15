b133887a169b89a76a98578cb323a7dc8968cb7d
omarsitoe <otrejome@vols.utk.edu>
2020-10-30 18:47:29 -0400

    removed excess instances of data - only every 5 seconds

--- merge.py
+++ merge.py
@@ -10,12 +10,18 @@ desired_features = ['Participant_ID', 'Age', 'Gender']
 df1 = pd.read_csv('Physiological Data.csv')
 df2 = pd.read_csv('Questionnaire Data.csv', usecols=desired_features)
 
+# Remove time feature
+df1 = df1.drop('Time', 1)
+
+# Reduce frequecy of treatment_time feature
+for idx, row in df1.iterrows():
+    if row['Treatment_Time']%5 != 0:
+        df1 = df1.drop(idx, 0)
+#print(df1)
+
 # Merge Gender and Age to DF1
 df3 = pd.merge(df1, df2)
-
-# Remove time feature
-df3 = df3.drop('Time', 1)
-
+#print(df3)
 
 # Read to csv file
 df3.to_csv('Merged.csv')
