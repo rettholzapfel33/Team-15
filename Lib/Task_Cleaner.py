4f379c60e1c404a5c3e23f276fd31388b9802f3a
omarsitoe <otrejome@vols.utk.edu>
2020-11-17 19:25:11 -0500

    replaced other missing values with -1

--- Lib/Task_Cleaner.py
+++ Lib/Task_Cleaner.py
@@ -11,16 +11,23 @@ filepath = path.abspath(path.join(basepath, "..", "Data", "Merged.csv"))
 
 #   Differnt CSV types  #
 file1 = open(filepath)
-file2 = open("New_Merged.csv", 'w')
+file2 = open("../Data/New_Merged.csv", 'w')
 reader = csv.reader(file1)
 writer = csv.writer(file2)
 
 #   Modify Csvs #
 for row in reader:
+    r5 = row[5]
+    r6 = row[6]
+    r7 = row[7]
+
     if row[5] == "":
-        writer.writerow([row[0], row[1], row[2], row[3], row[4], 'None', row[6], \
-                        row[7], row[8], row[9], row[10], row[11], row[12]])
-    else:
-        writer.writerow([row[0], row[1], row[2], row[3], row[4], row[5], row[6], \
-                        row[7], row[8], row[9], row[10], row[11], row[12]])
+        r5 = 'None'
+    if row[6] == "":
+        r6 = -1
+    if row[7] == "":
+        r7 = -1
+
+    writer.writerow([row[0], row[1], row[2], row[3], row[4], r5, r6, r7, \
+                    row[8], row[9], row[10], row[11], row[12]])
 
