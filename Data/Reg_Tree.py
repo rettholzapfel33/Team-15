ff042b0a1bebdeed4984f9dedac419fc5b74c16c
Chase Chaser <thunte11@vols.utk.edu>
2020-11-18 15:50:00 -0500

    Got some numbers. They suck. But it's numbers

--- Data/Reg_Tree.py
+++ Data/Reg_Tree.py
@@ -3,14 +3,22 @@
 #   and applies a regression tree to the data   #
 
 import csv
+import math
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from os import path
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeRegressor
 
 
 basepath = path.dirname(__file__)
 filepath = path.abspath(path.join(basepath, "..", "Data", "num-data.csv"))
 data = pd.read_csv(filepath)
+
+#   Loop through and if no chest_HR then match it to Wrist HR   #
+data = data.dropna(axis=0, how='any')
 
 #   Clear away uncessary columns #
 data = data.loc[:, ~data.columns.str.contains('^Unnamed')]
@@ -21,13 +29,21 @@ data = data.drop('Treatment_Time', axis=1)
 data = data.drop('PP_QC', axis=1)
 data = data.drop('EDA_QC', axis=1)
 data = data.drop('BR_QC', axis=1)
-data = data.drop('Chest_HR_QC', axis=1)
+data = data.drop('Wrist_HR_QC', axis=1)
 
 
+#   Data seperation #
+X = data.drop('Chest_HR_QC', axis=1)
+Y = data['Chest_HR_QC']
 
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.8, random_state=1)
 
+#   My ML model #
 
-print(data)
+regr_1 = DecisionTreeRegressor(max_depth=2)
+regr_1.fit(x_train, y_train)
+pred = regr_1.predict(x_test)
+print(regr_1.score(x_test, y_test))
 
 
 
