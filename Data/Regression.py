a1f020bc2633519b3f7fa5b21d75c6cbf89d3c68
jbridg12utk <joshua.bridges4@vols.utk.edu>
2020-11-19 21:33:36 -0500

    more functionality to regression

--- Data/Regression.py
+++ Data/Regression.py
@@ -6,7 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression, SGDRegressor
+from sklearn.linear_model import LinearRegression, SGDRegressor, Ridge
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def warn(*args, **kwargs):
@@ -15,43 +16,66 @@ import warnings
 warnings.warn = warn
 
 
-
-
-
 filename = '../Data/stand_num_Xs.csv'
+#filename = '../Data/num-data.csv'
 df = pd.read_csv(filename)
 
+#Drop Na values if using num-data.csv
+#df = df.dropna(axis=0, how='any')
 
 # Separate our features from our targets.
 
 #Total Features = ['Participant_ID','Group','Treatment','Treatment_Time','Task','PP_QC','EDA_QC','BR_QC','Age','Gender']
-features = ['Group','Treatment','Treatment_Time','EDA_QC','BR_QC']
+features = ['Participant_ID','Group','Treatment','Treatment_Time','Task','PP_QC','EDA_QC','BR_QC','Age','Gender']
+
 
 X = df[features]
 Y = df[['Chest_HR_QC']]
 
 #Kfold split
 
+tstVals = []
+alphas = [1e-3,1e-4,1e-5,1e-6,1e-7,1e-8]
 kf = KFold(n_splits = 10)
-for train_index, test_index in kf.split(range(len(X))):
-    x_train, x_test = X.values[train_index], X.values[test_index]
-    y_train, y_test = Y.values[train_index], Y.values[test_index]
-    y_train_converted = y_train.ravel()
-    y_test_converted = y_test.ravel()
+for alph in alphas:
+    max = -5
+    i = 1
+    for train_index, test_index in kf.split(range(len(X))):
+        x_train, x_test = X.values[train_index], X.values[test_index]
+        y_train, y_test = Y.values[train_index], Y.values[test_index]
+        y_train_converted = y_train.ravel()
+        y_test_converted = y_test.ravel()
 
-    #Train Models
-    linReg = LinearRegression(n_jobs = -2).fit(x_train, y_train_converted)
-    gradReg = SGDRegressor().fit(x_train, y_train_converted)
-
-    # Now, we run our test data through our trained models.
-    # = linReg.predict(x_test)
-    #predicted_grad = gradReg.predict(x_test)
+        #Train Models
+        linReg = LinearRegression(n_jobs = -2).fit(x_train, y_train_converted)
+        gradReg = SGDRegressor(loss= 'squared_loss', alpha = 1e-5, max_iter=2500).fit(x_train, y_train_converted)
+        ridgReg = Ridge().fit(x_train, y_train_converted)
+        
+        # Now, we run our test data through our trained models.
+        #predicted_lin= linReg.predict(x_test)
+        #predicted_grad = gradReg.predict(x_test)
+        
+     
+        linR2 = linReg.score(x_test, y_test_converted)
+        gradR2 = gradReg.score(x_test, y_test_converted)
+        ridgR2 = ridgReg.score(x_test, y_test_converted)
+        
+        if gradR2 > max:
+            max = gradR2
+            
+        print("Fold: " + str(i))
+        i+=1
+        print("Lin R2 Score \t\t-> " + str(linR2))
+        print("GradDes R2 Score \t-> " + str(gradR2))
+        print("Ridge R2 Score \t\t-> " + str(ridgR2))
+        print()
     
-    linR2 = linReg.score(x_test, y_test_converted)
-    gradR2 = gradReg.score(x_test, y_test_converted)
-    
-    print("Lin R2 Score \t\t-> " + str(linR2))
-    print("GradDes R2 Score \t-> " + str(gradR2))
-    print()
+    tstVals = np.append(tstVals, max)
 
+pl.plot(alphas, tstVals)
+pl.title("Max Gradient Descent R Squared for Different Alphas")
+pl.ylabel("Gradient Descent R Squared Score")
+pl.xlabel("Training Rate")
+pl.show()
+print(tstVals)
 
