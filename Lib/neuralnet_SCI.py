c8bfe369d3abec0b701c8e08437b7ba84443411a
Rett Holzapfel <rettholzapfel@gmail.com>
2020-11-22 15:47:57 -0600

    Adding Dr. William's Code

--- Lib/neuralnet_SCI.py
+++ Lib/neuralnet_SCI.py
@@ -7,10 +7,12 @@ from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential #type: ignore
 from keras.layers import Dense #type: ignore
 from sklearn.preprocessing import LabelBinarizer
-from keras.wrappers.scikit_learn import KerasClassifier #type: ignore
-from keras.backend.tensorflow_backend import set_session
+from keras.wrappers.scikit_learn import KerasRegressor #type: ignore
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
+from keras.layers.normalization import BatchNormalization
+from keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
 
 # (0) Hide as many warnings as possible!
 import os
@@ -26,7 +28,7 @@ heartRate = pd.read_csv('../Data/num-data.csv')
 heartRate = heartRate.dropna(axis=0, how='any')
 
 # Use data sampling to reduce the size of the data
-heartRate = heartRate.sample(n=5000, axis=0)
+heartRate = heartRate.sample(n=500, axis=0)
 
 # Split the data into features and targets
 features = ['Treatment', 'Treatment_Time',
@@ -54,54 +56,131 @@ X_test = pd.DataFrame(scaler.transform(X_test),
                            columns=X_test.columns,
                            index=X_test.index)
 
-# Build Keras Model 1 based on tuning
-def BaselineModel():
-    model = Sequential()
-    model.add(Dense(7, input_dim=7, activation='relu', name='layer_1'))
-    model.add(Dense(30, activation='relu', name='layer_2'))
-    model.add(Dense(10, activation='sigmoid', name='output_layer'))
-     
-    # Don't change this!
-    model.compile(loss="sparse_categorical_crossentropy",
-                  optimizer="adam",
-                  metrics=['accuracy'])
-    return model
 
-# Keras Model 2
-def AlternativeModel1():
-    model = Sequential()
-    model.add(Dense(7, input_dim=7, activation='relu', name='layer_1'))
-    model.add(Dense(10, activation='sigmoid', name='layer_2'))
-    model.add(Dense(5, activation='sigmoid', name='layer_3'))
-    model.add(Dense(10, activation='sigmoid', name='output_layer'))
-    
-    # Don't change this!
-    model.compile(loss="sparse_categorical_crossentropy",
-                  optimizer="adam",
-                  metrics=['accuracy'])
-    return model
+#-----------------------------------------------------------------------------
+# Define custom loss functions for regression in Keras 
+#-----------------------------------------------------------------------------
+
+# root mean squared error (rmse) for regression
+def rmse(y_true, y_pred):
+    from keras import backend
+    return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
+
+# mean squared error (mse) for regression
+def mse(y_true, y_pred):
+    from keras import backend
+    return backend.mean(backend.square(y_pred - y_true), axis=-1)
+
+# coefficient of determination (R^2) for regression
+def r_square(y_true, y_pred):
+    from keras import backend as K
+    SS_res =  K.sum(K.square(y_true - y_pred)) 
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
+    return (1 - SS_res/(SS_tot + K.epsilon()))
+
+def r_square_loss(y_true, y_pred):
+    from keras import backend as K
+    SS_res =  K.sum(K.square(y_true - y_pred)) 
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
+    return 1 - ( 1 - SS_res/(SS_tot + K.epsilon()))
+
+#-----------------------------------------------------------------------------
+# A Baseline Model
+#-----------------------------------------------------------------------------
+# built Keras sequential model 
+model = Sequential()
+# add batch normalization
+model.add(BatchNormalization())
+# add layer to the MLP for data (404,13) 
+model.add(Dense(units=7, activation='relu', input_dim=7))
+# add output layer
+model.add(Dense(units=1, activation='relu'))
+# compile regression model loss should be mean_squared_error //
+model.compile(optimizer="Nadam", loss="mean_squared_error", metrics=["mean_squared_error", rmse, r_square])
+# enable early stopping based on mean_squared_error
+earlystopping=EarlyStopping(monitor="mean_squared_error", patience=40, verbose=1, mode='auto')
+# fit model
+result = model.fit(np.array(X_train), np.array(y_train), epochs=240, batch_size=5, validation_data=(np.array(X_test), np.array(y_test)))
+# get predictions
+y_pred = model.predict(X_test)
+
+#-----------------------------------------------------------------------------
+# Plot learning curves including R^2 and RMSE
+#-----------------------------------------------------------------------------
+
+# plot training curve for R^2 (beware of scale, starts very low negative)
+plt.plot(result.history['val_r_square'])
+plt.plot(result.history['r_square'])
+plt.title('model R^2')
+plt.ylabel('R^2')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+           
+# plot training curve for rmse
+plt.plot(result.history['rmse'])
+plt.plot(result.history['val_rmse'])
+plt.title('rmse')
+plt.ylabel('rmse')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+import sklearn.metrics, math
+print("\n")
+print("Mean absolute error (MAE):      %f" % sklearn.metrics.mean_absolute_error(y_test,y_pred))
+print("Mean squared error (MSE):       %f" % sklearn.metrics.mean_squared_error(y_test,y_pred))
+print("Root mean squared error (RMSE): %f" % math.sqrt(sklearn.metrics.mean_squared_error(y_test,y_pred)))
+print("R square (R^2):                 %f" % sklearn.metrics.r2_score(y_test,y_pred))
 
 
-# Evaluate the models
+#-----------------------------------------------------------------------------
+# An Alternative Model
+#-----------------------------------------------------------------------------
+# built Keras sequential model 
+model = Sequential()
+# add batch normalization
+model.add(BatchNormalization())
+# add layer to the MLP for data (404,13) 
+model.add(Dense(units=7, activation='relu', input_dim=7))
+# a secpmd ;auer
+model.add(Dense(units=25, activation='relu', input_dim=7))
+# add output layer
+model.add(Dense(units=1, activation='relu'))
+# compile regression model loss should be mean_squared_error //
+model.compile(optimizer="Nadam", loss="mean_squared_error", metrics=["mean_squared_error", rmse, r_square])
+# enable early stopping based on mean_squared_error
+earlystopping=EarlyStopping(monitor="mean_squared_error", patience=40, verbose=1, mode='auto')
+# fit model
+result = model.fit(np.array(X_train), np.array(y_train), epochs=240, batch_size=5, validation_data=(np.array(X_test), np.array(y_test)))
+# get predictions
+y_pred = model.predict(X_test)
 
-# - - Model 1 - - 
-estimator = KerasClassifier(
-        build_fn=BaselineModel,
-        epochs=200, batch_size=20,
-        verbose=0)
-kfold = KFold(n_splits=5, shuffle=True)
-print("- - - - - - - - - - - - - ")
-for i in range(0,10):
-    results = cross_val_score(estimator, X_train, y_train, cv=kfold)
-    print("(MODEL 1 : RUN " + str(i) +") Performance: mean: %.2f%% std: (%.2f%%)" % (results.mean()*100, results.std()*100))
+#-----------------------------------------------------------------------------
+# Plot learning curves including R^2 and RMSE
+#-----------------------------------------------------------------------------
 
-# - - Model 2 - - 
-estimator = KerasClassifier(
-        build_fn=AlternativeModel1,
-        epochs=200, batch_size=20,
-        verbose=0)
-kfold = KFold(n_splits=5, shuffle=True)
-print("- - - - - - - - - - - - - ")
-for i in range(0,10):
-    results = cross_val_score(estimator, X_train, y_train, cv=kfold)
-    print("(MODEL 2 : RUN " + str(i) +") Performance: mean: %.2f%% std: (%.2f%%)" % (results.mean()*100, results.std()*100))
+# plot training curve for R^2 (beware of scale, starts very low negative)
+plt.plot(result.history['val_r_square'])
+plt.plot(result.history['r_square'])
+plt.title('model R^2')
+plt.ylabel('R^2')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+           
+# plot training curve for rmse
+plt.plot(result.history['rmse'])
+plt.plot(result.history['val_rmse'])
+plt.title('rmse')
+plt.ylabel('rmse')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+import sklearn.metrics, math
+print("\n")
+print("Mean absolute error (MAE):      %f" % sklearn.metrics.mean_absolute_error(y_test,y_pred))
+print("Mean squared error (MSE):       %f" % sklearn.metrics.mean_squared_error(y_test,y_pred))
+print("Root mean squared error (RMSE): %f" % math.sqrt(sklearn.metrics.mean_squared_error(y_test,y_pred)))
+print("R square (R^2):                 %f" % sklearn.metrics.r2_score(y_test,y_pred))
